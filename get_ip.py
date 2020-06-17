c71e4740851a8c82e222fba71c7cac294b942368
孙瑞川 <sunruichuan@inagora.cn>
2020-06-17 16:37:20 +0800

    增加获取内网ip方法

--- get_ip.py
+++ get_ip.py
@@ -4,9 +4,12 @@
 # @File    : get_ip.py
 
 import socket
+import requests
+from lxml import etree
 
 
-def get_host_ip():
+# 获取网卡ip
+def get_inner_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         s.connect(('8.8.8.8', 80))
@@ -16,6 +19,18 @@ def get_host_ip():
     return _ip
 
 
+# 解析网页，正则判断，获取用户实际公网ip
+def get_outer_ip():
+    url = 'http://tool.chinaz.com/'
+    res = requests.get(url=url)
+    html = etree.HTML(res.text)
+    _ip = html.xpath('/html/body/div[1]/div[5]/div[1]/a[2]')[0].text
+    _ip = str(_ip).split("：")[1]
+    return _ip
+
+
 if __name__ == '__main__':
-    ip = get_host_ip()
-    print(ip)
+    ip1 = get_inner_ip()
+    ip2 = get_outer_ip()
+    print(ip1)
+    print(ip2)
